ee5afca386d42087e5d3e9d34c63d1f24ee8d1ef
Bruno Mendes <brunomendes962@gmail.com>
2017-09-19 19:36:50 -0300

    comecando crud clinte e usuario

---
+++ src/classes/ContaPagar.java
@@ -0,0 +1,182 @@
+package classes;
+
+import classesDAO.ContaReceberDAO;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+
+public class ContaReceber {
+    private int cod; 
+    private int cod_cliente;
+    private String cliente;
+    private String data_vencimento;
+    private double valor;
+    private int parcela_atual;
+    private int parcela_total;  
+    //parcelas no formato que será mostrado na tabela
+    private String parecelas; 
+    private String status;
+    private String modo_pagamento;
+    private String obs;
+      
+    //criando tabela com os dados necessários
+    public void makeTable(JTable table, ArrayList<ContaReceber> recebimentos){
+         DefaultTableModel tbl = (DefaultTableModel) table.getModel();
+            tbl.setRowCount(0);
+            //ordem das colunas na tabela recebimentos:
+            // cod, cliente, data_venc, valor, parcela, status
+            for (int i = 0; i < recebimentos.size(); i++) {
+                
+                ContaReceber recebimento_atual = recebimentos.get(i);
+                
+                                
+                //colocando os recebimentos na tabela
+                Object[] dados = {
+                    recebimento_atual.getCod(),
+                    recebimento_atual.getCliente(),                    
+                    recebimento_atual.getValor(),
+                    recebimento_atual.getData_vencimento(),
+                    recebimento_atual.getParcela_total(),
+                    recebimento_atual.getModo_pagamento(),
+                    recebimento_atual.getStatus(),                                    
+                };
+                
+                //adicionando linha na tabela
+                tbl.addRow(dados);                
+            }
+    }
+    
+    //faz tabela com todos os recebimentos
+    public void todosRecebimentosTabela(JTable table){
+        
+        ArrayList<ContaReceber> recebimentos;
+        
+        try {
+            recebimentos = ContaReceberDAO.retornarTodosRecebimentos();
+             if(recebimentos.size() > 0){
+            this.makeTable(table, recebimentos);           
+        } 
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex);
+        }
+        
+       
+    }
+    
+    public void pesquisarTabela(JTable table, String pesquisa){
+        ArrayList<ContaReceber> recebimentos;
+        
+        recebimentos = ContaReceberDAO.pesquisarRecebimentos(pesquisa);
+        
+        if(recebimentos.size() > 0){
+           this.makeTable(table, recebimentos);          
+        
+        } else {
+            JOptionPane.showMessageDialog(null, "Nenhum registro encontrado");
+        } 
+        
+    }
+    
+    
+    //getters e setters
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public int getCod_cliente() {
+        return cod_cliente;
+    }
+
+    public void setCod_cliente(int cod_cliente) {
+        this.cod_cliente = cod_cliente;
+    }
+
+    public String getData_vencimento() {
+        return data_vencimento;
+    }
+
+    public void setData_vencimento(String data_vencimento) {
+        this.data_vencimento = data_vencimento;
+    }
+    
+     
+    //Data em formato string
+    public void setData_vencimentoString(String data)  {
+       
+    }
+    
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public int getParcela_atual() {
+        return parcela_atual;
+    }
+
+    public void setParcela_atual(int parcela_atual) {
+        this.parcela_atual = parcela_atual;
+    }
+
+    public int getParcela_total() {
+        return parcela_total;
+    }
+
+    public void setParcela_total(int parcela_total) {
+        this.parcela_total = parcela_total;
+    }
+
+    public String getParecelas() {
+        return parecelas;
+    }
+
+    public void setParecelas(String parecelas) {
+        this.parecelas = parecelas;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getModo_pagamento() {
+        return modo_pagamento;
+    }
+
+    public void setModo_pagamento(String modo_pagamento) {
+        this.modo_pagamento = modo_pagamento;
+    }
+
+    public String getObs() {
+        return obs;
+    }
+
+    public void setObs(String obs) {
+        this.obs = obs;
+    }
+}
---
+++ src/classesDAO/ContaPagarDAO.java
@@ -0,0 +1,161 @@
+package classesDAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import classes.ContaReceber;
+import java.util.ArrayList;
+
+public class ContaReceberDAO 
+{          
+   
+    private static PreparedStatement state = null;
+    private static ResultSet resultado = null;
+    
+    
+    //-----PESQUISAR
+    public static ArrayList<ContaReceber> pesquisarRecebimentos(String pesquisa){        
+        
+        ArrayList<ContaReceber> recebimentos = new ArrayList();
+        Connection con = Conexao.getConexao();
+        
+        String sql = "SELECT r.id, c.nome, r.data_vencimento ,r.valor, r.status, r.total_parcelas, r.modo_pagamento"
+                + "  FROM conta_receber as r, cliente as c "
+                + " WHERE r.id_cliente = c.id AND ( c.nome LIKE(?) OR status LIKE(?) OR modo_pagamento LIKE(?) );";
+        
+        try {
+            state = con.prepareStatement(sql);
+            state.setString(1, "%" + pesquisa + "%");
+            state.setString(2, "%" + pesquisa + "%");
+            state.setString(3, "%" + pesquisa + "%");
+            
+            resultado = state.executeQuery();
+            
+            while( resultado.next() ){
+                // cod, cliente, data_venc, valor, parcela, status
+                ContaReceber receber = new ContaReceber();
+                receber.setCod(resultado.getInt("id"));
+                receber.setCliente(resultado.getString("nome"));
+                receber.setData_vencimento(resultado.getString("data_vencimento"));
+                receber.setParcela_total(resultado.getInt("total_parcelas"));
+                receber.setValor(resultado.getDouble("valor"));
+                receber.setModo_pagamento(resultado.getString("modo_pagamento"));
+                receber.setStatus(resultado.getString("status"));
+                
+                recebimentos.add(receber);                
+            }
+            
+            Conexao.fecharConexao(con, state, resultado);
+            
+            return recebimentos;
+            
+        } catch (SQLException ex) {
+            Conexao.fecharConexao(con, state, resultado);
+            throw new RuntimeException("Erro: " + ex);
+            
+        }        
+    }
+    
+    //-----SELECT ALL
+    public static ArrayList<ContaReceber> retornarTodosRecebimentos() throws SQLException{        
+        ArrayList<ContaReceber> recebimentos = new ArrayList();
+        Connection con = Conexao.getConexao();
+        
+        String sql = "SELECT r.id, c.nome, r.data_vencimento ,r.valor, r.status, r.total_parcelas, r.modo_pagamento"
+                + "  FROM conta_receber as r, cliente as c "
+                + " WHERE r.id_cliente = c.id ORDER BY r.id; ";
+        
+       
+            state = con.prepareStatement(sql);
+            resultado = state.executeQuery();
+            
+            while(resultado.next()){
+                // cod, cliente, data_venc, valor, parcela, status
+                ContaReceber receber = new ContaReceber();
+                receber.setCod(resultado.getInt("id"));
+                receber.setCliente(resultado.getString("nome"));
+                receber.setData_vencimento( resultado.getString("data_vencimento") );
+                receber.setParcela_total(resultado.getInt("total_parcelas"));
+                receber.setValor(resultado.getDouble("valor"));
+                receber.setModo_pagamento(resultado.getString("modo_pagamento"));
+                receber.setStatus(resultado.getString("status"));
+                
+                recebimentos.add(receber);                
+            }
+            
+            Conexao.fecharConexao(con, state, resultado);
+            
+            return recebimentos;
+            
+             
+    }
+    
+    //----------iINSERIR RECEBIMENTO
+       public static boolean inserirRecebimento(ContaReceber recebimento) throws SQLException {
+            Connection  con = Conexao.getConexao();
+           String sql = "INSERT INTO conta_receber(id_cliente, valor, status , total_parcelas, modo_pagamento, obs ) values( ?, ?, ?, ?, ?, ?);"; 
+           state = con.prepareStatement(sql);
+           
+           state.setInt( 1,  recebimento.getCod_cliente() );
+           state.setDouble( 2,  recebimento.getValor() );
+           state.setString( 3,  "pendente" );
+           state.setInt( 4,  recebimento.getParcela_total() );
+           state.setString( 5,  recebimento.getModo_pagamento() );
+           state.setString( 6,  recebimento.getObs());
+                                 
+           state.execute();
+           Conexao.fecharConexao(con, state);
+           
+           return true;
+        }
+       
+       //----------ALTERAR RECEBIMENTO
+       public static boolean alterarRecebimento(ContaReceber recebimento) throws SQLException {
+            Connection  con = Conexao.getConexao();
+           String sql = "UPDATE conta_receber SET valor = ? , total_parcelas = ? ,  status = ?,  modo_pagamento = ?, obs = ? "
+                   + "WHERE id = ? ;"; 
+           state = con.prepareStatement(sql);
+           
+           state.setDouble( 1,  recebimento.getValor() );
+           state.setInt( 2,  recebimento.getParcela_total() );
+           state.setString( 3, recebimento.getStatus() );
+           state.setString( 4, recebimento.getModo_pagamento() );
+           state.setString( 5, recebimento.getObs() );
+           state.setInt( 6,  recebimento.getCod() );
+           
+                                 
+           state.execute();
+           Conexao.fecharConexao(con, state);
+           
+           return true;
+        }
+       
+       //------EXCLUIR    
+       public static boolean excluirRecebimento(int id) throws SQLException {
+            Connection  con = Conexao.getConexao();
+           String sql = "DELETE FROM conta_receber WHERE id = ? "; 
+           state = con.prepareStatement(sql);           
+           
+           state.setInt( 1,  id );
+           
+           state.execute();
+           Conexao.fecharConexao(con, state);
+           
+           return true;
+        }
+       
+       //-------CONFIRMAR RECEBIMENTO
+       public static boolean confirmarRecebimento(int cod) throws SQLException {
+           Connection  con = Conexao.getConexao();
+           String sql = "UPDATE conta_receber SET  status = 'Confirmado'  WHERE id = ? ;";
+           
+           state = con.prepareStatement(sql);
+           state.setInt(1, cod);
+           state.execute();
+           Conexao.fecharConexao(con, state);
+           
+           return true;
+        }
+    }
+
